0afd58717758f05f4f02d349c8f5f97fa2db0186
minorchy <minorchy@naver.com>
2024-05-13 22:15:03 +0900

    Initial commit

---
+++ src/main/java/com/myboard/entity/Board.java
@@ -0,0 +1,23 @@
+package com.myboard.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+}
